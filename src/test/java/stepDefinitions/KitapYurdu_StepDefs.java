c4ca386fbe73bfa94c36708922ec06b35ea8f629
HakanKara75 <hakanbatirhan@gmail.com>
2023-08-24 21:59:54 +0300

    kjl

---
+++ src/test/java/stepDefinitions/KitapYurdu_StepDefs.java
@@ -0,0 +1,37 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+import screens.androidScreen.KitapYurdu;
+import utils.Driver;
+
+import static org.junit.Assert.*;
+import static utils.ReusableMethods.scroll;
+
+public class KitapYurdu_StepDefs {
+
+
+    KitapYurdu kitap= new KitapYurdu();
+    @Given("Kitap Yurdu sitesine gidildi")
+    public void kitap_yurdu_sitesine_gidildi() {
+        Driver.getDriver();
+    }
+    @When("Sitede oldugu dogrulandi")
+    public void sitede_oldugu_dogrulandi() {
+        assertTrue(kitap.logo.isDisplayed());
+    }
+    @Then("Search ikonu tiklandi")
+    public void search_ikonu_tiklandi() {
+        kitap.searchImage.click();
+    }
+    @Then("SearchBox {string} kelimesi aratildi")
+    public void search_box_kelimesi_aratildi(String text) {
+        kitap.searchBox.sendKeys(text);
+    }
+    @Then("dagobert kelimesi oldugu dogrulandi")
+    public void dagobert_kelimesi_oldugu_dogrulandi() throws InterruptedException {
+
+        scroll(Driver.getDriver(), 1);
+        assertTrue(kitap.dagobert.isDisplayed());
+    }
+
+}
